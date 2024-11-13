df8e1c61f600abfc3180ee9067d54cf8596dae67
emersonschartner <42253298+emersonschartner@users.noreply.github.com>
2024-11-13 15:55:04 -0300

    Update main.tf

---
+++ terraform/S3-MED-STATIC/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+variable "bucket_name" {
+    type = string
+}
+
+resources "aws_s3_bucket" "static_site_bucket" {
+    bucket = "static-site-${var.bucket_name}"
+
+    website {
+        index_document = "index.html"
+        error_document = "error.html"
+
+    }
+
+    tags = {
+        name = "Static site Bucket"
+        Environment = "Production"
+    }
+}
+
+
+resources "aws_s3_bucket_public_access_block" "static_site_bucket" {
+    bucket = aws_s3_bucket.static_site_bucket.id
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
